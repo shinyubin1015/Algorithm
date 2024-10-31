2212510711cfe6f9621e7cbb590daa0957958767
shinyubin1015 <penutljh@gmail.com>
2024-11-01 08:13:37 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 112 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -1,17 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		for (int i = 0; i < str.length(); i++) {
+    public static void main(String[] args) throws IOException {
+    	InputStreamReader rd = new InputStreamReader(System.in);
+    	BufferedReader br = new BufferedReader(rd);
+    	String str = br.readLine();
+    	
+    	for (int i = 0; i < str.length(); i++) {
 			if ( Character.isUpperCase(str.charAt(i))) {
 				System.out.print(Character.toLowerCase(str.charAt(i)));
 			} else {
 				System.out.print(Character.toUpperCase(str.charAt(i)));
 			}
 		}
-	}
-
+    }
 }
