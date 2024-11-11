059fe65f95d5b13f42fff149b6f4866d4abcdaa0
shinyubin1015 <penutljh@gmail.com>
2024-11-12 08:30:10 +0900

    [Bronze II] Title: 단어의 개수, Time: 452 ms, Memory: 31216 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
-		String ns = str.trim();
-		if (ns.isEmpty()) {
-			System.out.println("0");
-		} else {
-			System.out.println(ns.split(" ").length);
-		}
+		StringTokenizer st = new StringTokenizer(str);
+		System.out.println(st.countTokens());
 	 }
 }
 
