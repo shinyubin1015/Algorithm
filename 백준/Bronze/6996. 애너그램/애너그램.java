f9f0bd769228ede2ec9fe9084346a2b3287608e6
shinyubin1015 <penutljh@gmail.com>
2024-11-21 08:48:57 +0900

    [Bronze I] Title: 애너그램, Time: 192 ms, Memory: 18540 KB -BaekjoonHub

--- 백준/Bronze/6996. 애너그램/애너그램.java
+++ 백준/Bronze/6996. 애너그램/애너그램.java
@@ -1,8 +1,19 @@
-import java.util.Arrays;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) {
+
+    private static boolean solveAnagrams(String first, String second ) {
+        char[] a = first.toCharArray();
+		char[] b = second.toCharArray();
+		Arrays.sort(a);
+		Arrays.sort(b);
+		return Arrays.equals(a, b);
+    }
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int numTests = sc.nextInt();
@@ -13,13 +24,5 @@ public class Main {
 
             System.out.println(first + " & " + second + " are " + (solveAnagrams(first, second) ? "anagrams." : "NOT anagrams."));
         }
-	}
-
-	private static boolean solveAnagrams(String first, String second) {
-		char[] a = first.toCharArray();
-		char[] b = second.toCharArray();
-		Arrays.sort(a);
-		Arrays.sort(b);
-		return Arrays.equals(a, b);
-	}
+    }
 }
